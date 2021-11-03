037a20bb8df48d966739f39a7db979815115cb6a
Fabio Lopez Vazquez <flopevaz@everis.com>
2021-11-03 14:07:32 +0100

    Commented code removed

--- src/main/java/com/fabio/nttdata/controller/TaskController.java
+++ src/main/java/com/fabio/nttdata/controller/TaskController.java
@@ -4,17 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabio.nttdata.model.Task;
-import com.fabio.nttdata.repository.TaskRepository;
 import com.fabio.nttdata.service.TaskService;
 
 @RestController
@@ -22,16 +21,7 @@ import com.fabio.nttdata.service.TaskService;
 public class TaskController {
 	
 	@Autowired
-	TaskService taskService;
-	
-	@Autowired
-	TaskRepository repository;
-	
-	
-//	@GetMapping("/{id}")
-//	public Object getTaskById(@PathVariable(name="id") String id ) {
-//		return repository.findById(id);
-//	}
+	private TaskService taskService;
 	
 	@GetMapping
 	public List<Task> getAllTasks() {
@@ -40,15 +30,10 @@ public class TaskController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<String> createTask(@RequestBody Task task) {
-		ResponseEntity<String> response = null;
-		try {
-			taskService.addTask(task);
-			response = new ResponseEntity<String>("Task created successfully",HttpStatus.CREATED);
-		} catch (Exception e) {
-			return new ResponseEntity<String>("Repository is unavailable",HttpStatus.SERVICE_UNAVAILABLE);
-		}
-		return response;
+	@ResponseStatus(HttpStatus.CREATED)
+	public Task createTask(@RequestBody Task task) {
+		return taskService.addTask(task);
+			
 	}
 	
 	@DeleteMapping
@@ -57,16 +42,9 @@ public class TaskController {
 	}
 	
 	@PutMapping
-	public ResponseEntity<Task> updateTaskStatus(@RequestBody Task modified_task) {
+	public Task updateTaskStatus(@RequestBody Task modified_task) {		
+		return taskService.modifyTask(modified_task);
 		
-		ResponseEntity<Task> response = null;
-		
-		if(taskService.modifyTask(modified_task).getId().equals(modified_task.getId()))
-			response = new ResponseEntity<>(modified_task,HttpStatus.CREATED);
-		else {
-			response = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-		}
-		return response;
 		
 	}
 	
